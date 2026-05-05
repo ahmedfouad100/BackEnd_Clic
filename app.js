68bfcc59799ada26ca934f9a0aa319e40089dc28
ahmedfouad100 <ahmed.fouadcis@gmail.com>
2026-05-05 19:31:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use("/categories", categoryRouter);
 app.use("/products", productRouter);
 
 // Serve uploaded files
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads', express.static('uploads'));
 app.use('/assets', express.static('assets'));
 /*
 // Health check
